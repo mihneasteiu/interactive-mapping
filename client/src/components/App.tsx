1482c7aa562da08180f3b605854056c7442edeb1
Furkan Baylav <furkanbaylav@Furkans-Laptop.local>
2024-11-06 16:44:33 -0500

    pair-prog w/@mihneasteiu docs: Added the pin image to the project.

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -13,17 +13,6 @@ import {
 // (for instance, /src/private/api_key.tsx)
 // import {API_KEY} from "./private/api_key"
 
-const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-};
-
-initializeApp(firebaseConfig);
-
 function App() {
   return (
     <div className="App">
