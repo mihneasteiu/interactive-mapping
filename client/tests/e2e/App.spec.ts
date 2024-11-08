583b546a1afcf8bb748f88f2a7c5bee1afbf6635
Furkan Baylav <furkanbaylav@Furkans-Laptop.local>
2024-11-08 12:15:32 -0500

    testing: Added the global setup and testing suite.

--- client/tests/e2e/App.spec.ts
+++ client/tests/e2e/App.spec.ts
@@ -1,63 +1,32 @@
 import { expect, test } from "@playwright/test";
-import { clearUser } from "../../src/utils/api";
+import {
+  clerk,
+  clerkSetup,
+  setupClerkTestingToken,
+} from "@clerk/testing/playwright";
 
-/**
-  The general shapes of tests in Playwright Test are:
-    1. Navigate to a URL
-    2. Interact with the page
-    3. Assert something about the page against your expectations
-  Look for this pattern in the tests below!
- */
-
-const SPOOF_UID = "mock-user-id";
-
-test.beforeEach(
-  "add spoof uid cookie to browser",
-  async ({ context, page }) => {
-    // - Add "uid" cookie to the browser context
-    await context.addCookies([
-      {
-        name: "uid",
-        value: SPOOF_UID,
-        url: "http://localhost:8000",
-      },
-    ]);
-
-    // wipe everything for this spoofed UID in the database.
-    await clearUser(SPOOF_UID);
-  }
-);
-
-/**
- * Don't worry about the "async" yet. We'll cover it in more detail
- * for the next sprint. For now, just think about "await" as something
- * you put before parts of your test that might take time to run,
- * like any interaction with the page.
- */
-test("on page load, I see the gearup screen and skip auth.", async ({
-  page,
-}) => {
-  // Notice: http, not https! Our front-end is not set up for HTTPs.
+test.beforeEach(async ({ page }) => {
+  await setupClerkTestingToken({
+    page,
+  });
   await page.goto("http://localhost:8000/");
-  await expect(page.getByLabel("Gearup Title")).toBeVisible();
-  // <i> with aria-label favorite-words-header should include the SPOOF_UID
-  await expect(page.getByLabel("user-header")).toContainText(SPOOF_UID);
+  await clerk.loaded({ page });
+  await clerk.signIn({
+    page,
+    signInParams: {
+      strategy: "password",
+      password: process.env.E2E_CLERK_USER_PASSWORD,
+      identifier: process.env.E2E_CLERK_USER_USERNAME,
+    },
+  });
 });
 
-test("I can add a word to my favorites list", async ({ page }) => {
-  await page.goto("http://localhost:8000/");
-  // - get the <p> elements inside the <ul> with aria-label="favorite-words"
-  const favoriteWords = await page.getByLabel("favorite-words");
-  await expect(favoriteWords).not.toContainText("hello");
+test.afterEach(async ({ page }) => {
+  await clerk.signOut({ page });
+});
 
-  await page.getByLabel("word-input").fill("hello");
-  await page.getByLabel("add-word-button").click();
-
-  const favoriteWordsAfter = await page.getByLabel("favorite-words");
-  await expect(favoriteWordsAfter).toContainText("hello");
-
-  // .. and this works on refresh
-  await page.reload();
-  const favoriteWordsAfterReload = await page.getByLabel("favorite-words");
-  await expect(favoriteWordsAfterReload).toContainText("hello");
+test("I see the basic labels when I open the project", async ({ page }) => {
+  await expect(page.getByLabel("Sprint 5.1")).toBeVisible;
+  await expect(page.getByLabel("Sign out")).toBeVisible;
+  await expect(page.getByLabel("Clear pins")).toBeVisible;
 });
